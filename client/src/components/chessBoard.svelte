42088235dad2b775a69902d6a9c62f6b70592630
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-03-20 10:45:45 +0200

    started client

---
+++ client/src/components/chessBoard.svelte
@@ -0,0 +1,24 @@
+<script>
+	let board = [
+		['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+		['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+		['', '', '', '', '', '', '', ''],
+		['', '', '', '', '', '', '', ''],
+		['', '', '', '', '', '', '', ''],
+		['', '', '', '', '', '', '', ''],
+		['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+		['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+	];
+</script>
+
+<div class="grid grid-rows-8 grid-cols-8 w-[600px] h-[600px]">
+	{#each board as row, i}
+		{#each row as square, j}
+        <div class={i + j % 2 == 0 ? "bg-slate-300" : "bg-green-800"}>
+            <img src={`%sveltekit.assets%/pieces/${square}.png`} alt="">
+
+        </div>
+
+        {/each}
+	{/each}
+</div>
