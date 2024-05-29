4c27f9b365281c6f8acdc9e2f17558a9b7f247d5
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-05-30 02:33:11 +0300

    init

--- server/src/database/__init__.py
+++ server/src/database/__init__.py
@@ -3,21 +3,40 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-url = URL.create(
-    drivername="postgresql",
-    username="domnuraul",
-    password="1234",
-    host="localhost",
-    database="chessdb",
-    port=5432,
-)
-
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000, os
 
 class Base(DeclarativeBase):
     pass
 
+instance_connection_name = os.environ[
+    "INSTANCE_CONNECTION_NAME"
+]  # e.g. 'project:region:instance'
+db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
 
-engine = create_engine(url)
+ip_type = IPTypes.PUBLIC
+
+connector = Connector()
+
+def __getconn() -> pg8000.dbapi.Connection:
+    conn: pg8000.dbapi.Connection = connector.connect(
+        instance_connection_name,
+        "pg8000",
+        user=db_user,
+        password=db_pass,
+        db=db_name,
+        ip_type=ip_type,
+    )
+    return conn
+
+
+engine = create_engine(
+    "postgresql+pg8000://",
+    creator=__getconn,
+)
+
 SessionLocal = sessionmaker(bind=engine)
 
 
