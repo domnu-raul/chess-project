db7196bf20e0d399b41fd3fe224032c8a577bf09
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-06-10 04:15:03 +0300

    finished the animation

--- client/src/components/Form/Form.vue
+++ client/src/components/Form/Form.vue
@@ -2,53 +2,77 @@
 import FormHeader from "./FormHeader.vue";
 import FormInput from "./FormInput.vue";
 import ToggleButton from "./ToggleButton.vue";
-import { Transition } from "vue";
+import { Transition, watchEffect } from "vue";
 import { ref } from "vue";
 
 let username = ref("");
 let password = ref("");
 let confirmPassword = ref("");
 let isMember = ref(false);
+// if isMember was false and is now true, slide form in
+// if isMember was true and is now false, slide form out
+let lastIsMember = ref(false);
+let slideFormIn = ref(false);
+let slideFormOut = ref(false);
+
+watchEffect(() => {
+  if (isMember.value !== lastIsMember.value) {
+    if (isMember.value) {
+      slideFormIn.value = true;
+      slideFormOut.value = false;
+    } else {
+      slideFormIn.value = false;
+      slideFormOut.value = true;
+    }
+
+    lastIsMember.value = isMember.value;
+  }
+});
 </script>
 
 <template>
-  <Transition name="flex-slide">
-    <form class="flex flex-col gap-4 bg-zinc-900 rounded-2xl p-5">
-      <FormHeader />
+  <form
+    id="form"
+    class="flex flex-col gap-4 bg-zinc-900 rounded-2xl p-5"
+    :class="[
+      { 'slide-form-in': slideFormIn },
+      { 'slide-form-out': slideFormOut },
+    ]"
+  >
+    <FormHeader />
+    <FormInput
+      placeholder="Username"
+      type="text"
+      name="username"
+      v-model="username"
+    />
+    <FormInput
+      placeholder="Password"
+      type="password"
+      name="password"
+      v-model="password"
+      class="z-[1]"
+    />
+    <TransitionGroup name="slide-fade">
       <FormInput
-        placeholder="Username"
-        type="text"
-        name="username"
-        v-model="username"
-      />
-      <FormInput
-        placeholder="Password"
+        v-if="!isMember"
+        placeholder="Confirm Password"
         type="password"
-        name="password"
-        v-model="password"
-        class="z-[1]"
+        name="confirm-password"
+        v-model="confirmPassword"
       />
-      <TransitionGroup name="slide-fade">
-        <FormInput
-          v-if="!isMember"
-          placeholder="Confirm Password"
-          type="password"
-          name="confirm-password"
-          v-model="confirmPassword"
-        />
-        <button
-          type="submit"
-          class="bg-green-700 font-roboto font-black text-3xl text-slate-100 w-full py-4 rounded-lg cursor-default hover:bg-green-800 hover:text-slate-50"
-        >
-          Signup
-        </button>
-        <div class="flex flex-row justify-between items-center">
-          <p class="font-roboto text-xl text-slate-100">Already a member?</p>
-          <ToggleButton v-model="isMember" id="is-member-checkbox" />
-        </div>
-      </TransitionGroup>
-    </form>
-  </Transition>
+      <button
+        type="submit"
+        class="bg-green-700 font-roboto font-black text-3xl text-slate-100 w-full py-4 rounded-lg cursor-default hover:bg-green-800 hover:text-slate-50"
+      >
+        Signup
+      </button>
+      <div class="flex flex-row justify-between items-center">
+        <p class="font-roboto text-xl text-slate-100">Already a member?</p>
+        <ToggleButton v-model="isMember" id="is-member-checkbox" />
+      </div>
+    </TransitionGroup>
+  </form>
 </template>
 
 <style scoped>
@@ -66,22 +90,34 @@ let isMember = ref(false);
   opacity: 0%;
 }
 
-.slide-fade-enter-to {
-  opacity: 100%;
-}
-
-.slide-fade-enter-from {
-  opacity: 100%;
-}
-
 .slide-fade-leave-active {
   position: absolute;
 }
 
-.flex-slide-enter-active,
-.flex-slide-leave-active {
-  transition: flex-grow 0.3s ease-in-out, flex-shrink 0.3s ease-in-out;
-  max-height: min-content;
+@keyframes slideFormIn {
+  from {
+    min-height: 95%;
+  }
+  to {
+    min-height: calc(95% - 4.5rem)
+  }
 }
 
+@keyframes slideFormOut {
+  from {
+    max-height: calc(95% - 4.5rem)
+  }
+
+  to {
+    max-height: 100%;
+  }
+}
+
+.slide-form-in {
+  animation: slideFormIn 0.3s ease-in;
+}
+
+.slide-form-out {
+  animation: slideFormOut 0.3s ease-out;
+}
 </style>
