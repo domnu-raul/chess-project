bf6df383928c28ea9df178237b83d592e1a89bf6
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-06-13 17:08:05 +0300

    User home left pane

---
+++ client/src/components/Button.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        required: true
+    },
+    classValue: {
+        type: String,
+        required: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    onClick: {
+        type: Function,
+        default: () => () => { }
+    }
+})
+</script>
+
+<template>
+    <button :type="type"
+        class="bg-green-700 font-roboto font-black text-3xl text-slate-100 w-full py-4 rounded-lg cursor-default hover:bg-green-800 hover:text-slate-50 select-none"
+        :disabled="disabled" @click="onClick" :class="classValue">
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/UserProfile.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    user: {
+        type: Object,
+        required: false,
+    },
+});
+</script>
+
+<template>
+    <div class="grid grid-cols-2 bg-sky-600 rounded-lg">
+        <div class="flex flex-col p-3">
+            <h1
+                class="text-4xl my-auto leading-relaxed text-slate-100 font-robotoslab font-bold cursor-default select-none">
+                Username
+            </h1>
+            <img src="https://i.imgur.com/KjTdWGH.png" class="w-full rounded-md">
+        </div>
+        <div class="flex flex-col p-3 justify-end leading-10 text-slate-100 font-roboto font-medium">
+            <p>Elo N/A</p>
+            <p>Games played: N/A</p>
+            <p>Victories: N/A</p>
+            <p>Losses: N/A</p>
+            <p>Draws: N/A</p>
+        </div>
+
+    </div>
+</template>
+
+<style scoped></style>
